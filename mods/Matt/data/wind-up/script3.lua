37b1854e5e5a87b426665e21eb70a3e75a4e4bd0
AmorAltra104 <amongaltra@gmail.com>
2023-04-16 14:44:39 +0200

    f

--- mods/Matt/data/wind-up/script3.lua
+++
@@ -1,5 +0,0 @@
-function opponentNoteHit(index, nData, nType, sustain)
-	if getPropertyFromClass('ClientPrefs', 'noteSplashes') and not sustain then
-		runHaxeCode('game.spawnNoteSplash('..getPropertyFromGroup('strumLineNotes', nData, 'x')..', '..getPropertyFromGroup('strumLineNotes', nData, 'y')..', '..nData..');')
-	end
-end
